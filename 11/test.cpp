391ba7ff37d654a9d69bd760085feacc6bc7d8c1
lokmos <23110240153@m.fudan.edu.cn>
2024-07-15 19:15:36 +0800

    Chapter11 finished

--- 11/test.cpp
+++ 11/test.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
@@ -12,5 +13,34 @@ void E11_12() {
 
     while (cin >> str >> x) {
         vec.push_back(make_pair(str, x));
+    }
+}
+
+void E11_13() {
+    vector<pair<string, int>> vec;
+    string str;
+    int x;
+    
+    // Method 1
+    while (cin >> str >> x) {
+        vec.push_back(pair<string, int>(str, x));
+    }
+
+    // Method 2
+    while (cin >> str >> x) {
+        vec.push_back({str, x});
+    }
+
+    // Method 3
+    while (cin >> str >> x) {
+        vec.emplace_back(str, x);
+    }
+}
+
+void E11_20() {
+    unordered_map<string, int> word_count;
+    string word;
+    while (cin >> word) {
+        ++word_count[word];
     }
 }
