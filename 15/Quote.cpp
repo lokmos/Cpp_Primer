e13bebc50c3305e7b46db3eead63b82af314352c
lokmos <23110240153@m.fudan.edu.cn>
2024-08-06 16:48:33 +0800

    Chapter15 Update

---
+++ 15/Quote.cpp
@@ -0,0 +1,9 @@
+#include "Quote.h"
+#include <iostream>
+using namespace std;
+
+double print_total(ostream &os, const Quote &item, size_t n) {
+    double ret = item.net_price(n);
+    os << "ISBN: " << item.isbn() << " # sold: " << n << " total due: " << ret << endl;
+    return ret;
+}
---
+++ 15/Quote.h
@@ -0,0 +1,37 @@
+#ifndef QUOTE_H
+#define QUOTE_H
+
+#include <string>
+
+class Quote {
+public:
+    Quote() = default;
+    Quote(const std::string &book, double sales_price) : bookNo(book), price(sales_price) {}
+    std::string isbn() const;   
+    virtual double net_price(std::size_t n) const {return n * price;}
+    virtual ~Quote() = default;
+private:
+    std::string bookNo;
+protected:
+    double price = 0.0;
+};
+
+class Bulk_quote : public Quote {
+public:
+    Bulk_quote() = default;
+    Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) : 
+        Quote(book, p), min_qty(qty), discount(disc) {}
+    double net_price(std::size_t n) const override {
+        if (n >= min_qty)
+            return n * (1 - discount) * price;
+        else
+            return n * price;
+    }
+private:
+    std::size_t min_qty = 0;
+    double discount = 0.0;
+};
+
+double print_total(std::ostream &os, const Quote &item, std::size_t n);
+
+#endif
