160c60760db32175fd68b9211607d20dc7b7377f
lokmos <23110240153@m.fudan.edu.cn>
2024-08-12 16:06:38 +0800

    Chapter15 update

---
+++ 15/Basket.cpp
@@ -0,0 +1,11 @@
+#include "Basket.h"
+using namespace std;
+
+double Basket::total_receipt(ostream &os) const {
+    double sum = 0.0;
+    for (auto iter = items.cbegin(); iter != items.cend(); iter = items.upper_bound(*iter)) {
+        sum += print_total(os, **iter, items.count(*iter));
+    }
+    os << "Total Sale: " << sum << endl;
+    return sum;
+}
