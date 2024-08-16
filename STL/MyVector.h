4a50fa68b700573a64b24fe8dded024d88f2d7d1
lokmos <23110240153@m.fudan.edu.cn>
2024-08-16 15:42:15 +0800

    STL

--- STL/MyVector.h
+++ STL/MyVector.h
@@ -1,6 +1,17 @@
 #ifndef MYVECTOR_H
 #define MYVECTOR_H
 
-class 
+template <typename T>
+class Vector {
+private:
+    T* elements;
+    size_t size;
+    size_t capacity;
+public:
+    // 构造函数
+    Vector() : elements(nullptr), size(0), capacity(0) {};
+
+    
+};
 
 #endif
