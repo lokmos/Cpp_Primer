53d93c8647f2ad8b2c78c70e8dd1b1683e42a019
lokmos <23110240153@m.fudan.edu.cn>
2024-08-17 16:02:20 +0800

    stl

---
+++ STL/MyList.h
@@ -0,0 +1,165 @@
+#ifndef MYLIST_H
+#define MYLIST_H
+
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+
+template <typename T>
+class List {
+public:
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& os, const List<U>& pt);
+
+private:
+    // 定义节点结构
+    struct Node {
+        T data;
+        T* prev;
+        T* next;
+
+        Node() : data(T()), prev(nullptr), next(nullptr) {}
+        Node(const T& value, Node* prevNode = nullptr, Node* nextNode = nullptr)
+            : data(value), prev(prevNode), next(nextNode) {}
+    };
+
+    Node* head;
+    Node* tail;
+    size_t size;
+
+public:
+    List() : head(nullptr), tail(nullptr), size(0) {}
+    ~List() {clear();}
+
+    void push_back(const T& value) {
+        Node* newNode = new Node(value, tail);
+        if (tail) {
+            tail->next = newNode;
+        } 
+        else {
+            head = newNode;
+        }
+        tail = newNode;
+        ++size;
+    }
+
+    void push_front(const T& value) {
+        Node* newNode = new Node(value, nullptr, head);
+        if (head) {
+            head->prev = newNode;
+        }
+        else {
+            tail = newNode;
+        }
+        head = newNode;
+        ++size;
+    }
+
+    size_t getSize() const {return size;}
+
+    T& operator[](size_t index) {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        Node* current = head;
+        for (size_t i = 0; i < index; ++i) {
+            current = current->next;
+        }
+        return current->data;
+    }
+
+    const T& operator[](size_t index) const {
+        if (index >= size) {
+            throw std::out_of_range("Index out of range");
+        }
+        Node* current = head;
+        for (size_t i = 0; i < index; ++i) {
+            current = current->next;
+        }
+        return current->data;
+    }
+
+    void pop_back() {
+        if (size > 0) {
+            Node* tmp = tail->prev;
+            delete tail;
+            tail = tmp;
+
+            if (tail) {
+                tail->next = nullptr;
+            }
+            else {
+                head = nullptr;
+            }
+            --size;
+        }
+    }
+
+    void pop_front() {
+        if (size > 0) {
+            Node *tmp = head->next;
+            delete head;
+            head = tmp;
+
+            if (head) {
+                head->prev = nullptr;
+            }
+            else {
+                tail = nullptr;
+            }
+            --size;
+        }
+    }
+
+    Node* getNode(const T& val) {
+        Node* current = head;
+        while (current) {
+            if (current->data == val) {
+                return current;
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
+
+    T* find(const T& val) {
+        Node* current = getNode(val);
+        if (current) {
+            return &current->data;
+        }
+        return nullptr;
+    }
+
+    void remove(const T& val) {
+        Node* current = getNode(val);
+        if (!current) {
+            return;
+        }
+
+        if (current == head) {
+            pop_front();
+        }
+        else if (current == tail) {
+            pop_back();
+        }
+        else {
+            current->prev->next = current->next;
+            current->next->prev = current->prev;
+            delete current;
+            --size;
+        }
+    }
+
+    bool empty() const {return size == 0;}
+
+    void clear() {
+        while (size > 0) {
+            pop_front();
+        }
+    }
+
+    Node* begin() {return head;}
+    
+};
+
+#endif
